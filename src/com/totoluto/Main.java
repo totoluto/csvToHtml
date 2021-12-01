eba8cac7497f705cf48854d336d8f0a039e00b00
totoluto <totoluto.development@protonmail.com>
2021-12-01 10:54:42 +0100

    initial commit

---
+++ src/com/totoluto/Main.java
@@ -0,0 +1,50 @@
+/**
+ * @project: csvToHtml
+ * @author: totoluto
+ * @date: 28.11.2021
+ * @version: 1.0.1
+ */
+
+
+
+package com.totoluto;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        FileReader csvReader = new FileReader("data.csv", StandardCharsets.UTF_8); // data.csv is produced by https://generatedata.com
+        BufferedReader bReader = new BufferedReader(csvReader);
+        String row;
+        int counter = 0;
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt", StandardCharsets.UTF_8, false));
+        while ((row = bReader.readLine()) != null) {
+            String[] data = row.split(",");
+
+            if(counter == 0){
+                writer.write("<tr>\n" +
+                        "\t <th>"+ data[0] + "</th>\n" + // first data column
+                        "\t <th>"+ data[1] +"</th>\n" + // second data column
+                        "\t <th>"+ data[2] +"</th>\n" + // third data column
+                        "\t <th>"+ data[3] +"</th>\n" + // fourth data column
+                        // Add more <th> and count data[n] up
+                        "</tr>\n");
+            }else{
+                writer.write("<tr>\n" +
+                        "\t <td>"+ data[0] + "</td>\n" + // first data column
+                        "\t <td>"+ data[1] +"</td>\n" + // second data column
+                        "\t <td>"+ data[2] +"</td>\n" + // third data column
+                        "\t <td>"+ data[3] +"</td>\n" + // fourth data column
+                        //Add more <td> and count data[n] up (adjust it to how many rows you have)
+                        "</tr>\n");
+            }
+            counter++;
+        }
+        csvReader.close();
+        writer.close();
+    }
+}
